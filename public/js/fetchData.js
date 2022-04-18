d6c7c442289d6f9cbd328076a6f91b5e31aae254
mahesago <marianhsg@gmail.com>
2022-04-18 04:14:57 -0300

    Changes on profile, navbar, footer, cars info and pictures

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -4,66 +4,84 @@ function loadCarsData() {
     return res.json();
   })
   .then(data => {
-    console.log(data);
 
-    data.forEach(function (current){
+    data.forEach((current)=>{
+        console.log(current.carId);
         let badge = document.createElement('div');
         badge.innerHTML = `
         <div class="car-wrapper">
-        <div>
-          <h3 class="title"> ${current.brand} ${current.model} </h3>
-          <div class="swiper">
-          <div class="swiper-wrapper">
-          <div class="swiper-slide" id="swiper">
-              <img src="${current.pics[0]}" width="200" height="200" alt="${current.brand}"/>
-              </div>
-              <div class="swiper-slide"><img src="${current.pics[1]}" width="200" height="200" alt="${current.brand}"/></div>
-              <div class="swiper-slide"><img src="${current.pics[2]}" width="200" height="200" alt="${current.brand}"/></div>
-              </div>
-              <div class="swiper-button-prev"></div>
-              <div class="swiper-button-next"></div>
+
+        <div class="slider-wrapper">
+          <h3 class="title-cars"> ${current.category} </h3>
+        <div id="carouselControl${current.carId}" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block swiper" src="${current.pics[0]}" alt="${current.brand}">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block swiper" src="${current.pics[1]}" alt="${current.brand}">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block swiper" src="${current.pics[2]}" alt="${current.brand}">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselControl${current.carId}" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          </a>
+          <a class="carousel-control-next" href="#carouselControl${current.carId}" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          </a>
         </div>
-        <div class="options-only-phone">${current.priceDay}</div>
+        <div class="options-price">$${current.priceDay} USD p/day</div>
         </div>
           <div class="details">
           <h3>Details</h3>
+          <p>${current.brand} ${current.model} or similar </p>
           <p>
-          2 Baggages
+          ${current.baggages}  Baggages
           </p>
           <p>
-          5 passengers
+          ${current.passengers}  passengers
           </p>
           <p>
-          Automatic
+          Air conditioning / Heating
           </p>
           <p>
-          Air conditioning
+          Pet friendly
           </p>
           </div>
           <div class="rate-details">
           <h3>Rate Details</h3>
           <ul>
-          <li>
-          Unlimited Mileage
-          </li>
-          <li>
-          Vehicle Assistance Service (mechanical defect)
-          </li>
-          <li>
-          All Local Taxes and Fees
-          </li>
-          <li>
-            One Additional Driver
-          </li>
-          <li>
-          Third Party Liability Insurance $1Million , With No Deductible- (EP/LIS/TPL)
-          </li>
-          <li>
-          Full Coverage of Rental Vehicle, With No Deductible: Accident & Theft (CDW/LDW)
-          </li>
-          <li>
-          No Cancellation Fee</ul>
-          </li>
+            <li>
+            Unlimited Mileage
+            </li>
+            <li>
+            Vehicle Assistance Service (mechanical defect)
+            </li>
+            <li>
+            All Local Taxes and Fees
+            </li>
+            <li>
+              One Additional Driver
+            </li>
+            <li>
+            Third Party Liability Insurance $1Million , With No Deductible- (EP/LIS/TPL)
+            </li>
+            <li>
+            Full Coverage of Rental Vehicle, With No Deductible: Accident & Theft (CDW/LDW)
+            </li>
+            <li>
+            No Cancellation Fee
+            </li>
+            <li>
+            Photos are for reference only
+            </li>
+            <li>
+            A similar car with the same characteristics can be provided
+            </li>
+          </ul>
+          <a class="route-link reservation-cta" href="/reservations" >Reserve now!</a>
           </div>
           </div>
           `;
